776401bcf47384d6dbddcb6c65b1baee1e59cb73
Duncan Procter <dprocter@gmail.com>
2022-10-04 15:52:25 +0100

    removing duplicate get_lable code

--- utils/get_labels.py
+++ utils/get_labels.py
@@ -12,26 +12,6 @@ def get_labels(file_path: str, subset: int = 0):
     """
 
     lab_pd = pd.read_csv(file_path)
-    lab_np = (
-        lab_pd[
-            [
-                "Bald",
-                "Black_Hair",
-                "Blond_Hair",
-                "Brown_Hair",
-                "Gray_Hair",
-                "Smiling",
-                "Straight_Hair",
-                "Wavy_Hair",
-                "Wearing_Hat",
-                "Wearing_Earrings",
-                "Wearing_Necktie",
-            ]
-        ]
-        .to_numpy()
-        .astype("int")
-        + 1
-    ) / 2
 
     if subset != 0:
         lab_np = (
