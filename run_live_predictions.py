ca2ddb8a084a413003930450df9b771d57a89fc1
Duncan Procter <dprocter@gmail.com>
2022-10-28 07:22:54 +0100

    tidying the repo
    some basic tidying changes and a readme

--- run_live_predictions.py
+++ run_live_predictions.py
@@ -19,6 +19,16 @@ def run_live_predictions(camera_path: str, resize_path: str, prediction_path: st
 
     for i in range(0,num_iterations):
 
+        out_num = ""
+        if i < 10:
+            out_num += f"000{i}"
+        elif i > 9 and i < 100:
+            out_num += f"00{i}"
+        elif i > 99:
+            out_num += f"0{i}"
+        else:
+            out_num += f"{i}"
+
         clear_folder(f"{resize_path}/images_resized/")
 
         resize(camera_path, f"{resize_path}/images_resized/")
@@ -39,13 +49,13 @@ def run_live_predictions(camera_path: str, resize_path: str, prediction_path: st
                         "Eyeglasses"
                     ])
 
-        out.to_csv(f"{prediction_path}predictions{i}.csv")
+        out.to_csv(f"{prediction_path}predictions{out_num}.csv")
 
         lookalikes = get_predictions(resize_path, lookalike_model_path)
 
-        pd.DataFrame(lookalikes).to_csv(f"{lookalike_path}predictions{i}.csv")
+        pd.DataFrame(lookalikes).to_csv(f"{lookalike_path}predictions{out_num}.csv")
 
-        get_eigenface(directory_in=f"{resize_path}/images_resized", directory_out=eigenface_path, iter = i)
+        get_eigenface(directory_in=f"{resize_path}/images_resized", directory_out=eigenface_path, iter = out_num)
 
-        time.sleep(5)
+        time.sleep(1)
 
